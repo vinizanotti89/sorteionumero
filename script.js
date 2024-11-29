b77d9985c00d318d91c2d614e60a8fa8fac4966f
Vinícius Zanotti de Oliveira <130515950+vinizanotti89@users.noreply.github.com>
2024-11-29 01:25:12 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,15 +3,16 @@ function generateNumber(){
     const min = Math.ceil(document.querySelector(".min").value)
     const max = Math.floor(document.querySelector(".max").value)
 
-
+    if(max > min){
     const result = Math.floor(Math.random() * (max - min + 1)) + min;
 
     alert(result)
-
+    }else {
+        alert ("O valor mínimo tem que ser MENOR que o valor máximo")
+    }
     
 
 }
-
 
 
 
