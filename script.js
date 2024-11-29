faa89fb16b5ef0bca7da2ea9ad93132bc3043828
Vinícius Zanotti de Oliveira <130515950+vinizanotti89@users.noreply.github.com>
2024-11-29 00:44:51 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+function generateNumber(){
+
+    const min = Math.ceil(document.querySelector(".min").value)
+    const max = Math.floor(document.querySelector(".max").value)
+
+
+    const result = Math.floor(Math.random() * (max - min + 1)) + min;
+
+    alert(result)
+
+    
+
+}
+
+
+
+
